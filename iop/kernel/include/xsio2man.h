fd34855e5c267f9181fc27267e890e4213632597
Ziemas <ziemas@ziemas.se>
2022-08-05 17:26:34 +0200

    sio2man

--- iop/kernel/include/xsio2man.h
+++ iop/kernel/include/xsio2man.h
@@ -36,22 +36,4 @@ int sio2_mtap_get_slot_max(int port);
 int sio2_mtap_get_slot_max2(int port);
 void sio2_mtap_update_slots(void);
 
-#define xsio2man_IMPORTS_start DECLARE_IMPORT_TABLE(sio2man, 1, 2)
-#define xsio2man_IMPORTS_end END_IMPORT_TABLE
-
-#ifndef NO_XSIO2MAN_IMPORTS
-#define I_sio2_transfer_reset DECLARE_IMPORT(26, sio2_transfer_reset)
-#define I_sio2_mtap_transfer_init DECLARE_IMPORT(48, sio2_mtap_transfer_init)
-#define I_sio2_transfer2 DECLARE_IMPORT(49, sio2_transfer2)
-#define I_sio2_transfer_reset2 DECLARE_IMPORT(50, sio2_transfer_reset2)
-#define I_sio2_mtap_change_slot_set DECLARE_IMPORT(51, sio2_mtap_change_slot_set)
-#define I_sio2_mtap_get_slot_max_set DECLARE_IMPORT(52, sio2_mtap_get_slot_max_set)
-#define I_sio2_mtap_get_slot_max2_set DECLARE_IMPORT(53, sio2_mtap_get_slot_max2_set)
-#define I_sio2_mtap_update_slots_set DECLARE_IMPORT(54, sio2_mtap_update_slots_set)
-#define I_sio2_mtap_change_slot DECLARE_IMPORT(55, sio2_mtap_change_slot)
-#define I_sio2_mtap_get_slot_max DECLARE_IMPORT(56, sio2_mtap_get_slot_max)
-#define I_sio2_mtap_get_slot_max2 DECLARE_IMPORT(57, sio2_mtap_get_slot_max2)
-#define I_sio2_mtap_update_slots DECLARE_IMPORT(58, sio2_mtap_update_slots)
-#endif
-
 #endif /* __XSIO2MAN_H__ */
