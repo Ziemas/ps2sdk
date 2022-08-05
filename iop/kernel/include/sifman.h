2210cbbc916d5669d3a8c50beb10f2a7a128f8eb
Ziemas <ziemas@ziemas.se>
2022-08-05 10:22:31 +0200

    sifman

--- iop/kernel/include/sifman.h
+++ iop/kernel/include/sifman.h
@@ -139,37 +139,4 @@ void sceSifResetDmaIntrHandler();
 
 unsigned int sceSifSetDmaIntr(SifDmaTransfer_t *dmat, int len, void (*func)(), void *data);
 
-#define sifman_IMPORTS_start DECLARE_IMPORT_TABLE(sifman, 1, 1)
-#define sifman_IMPORTS_end END_IMPORT_TABLE
-
-#define I_sceSifDma2Init DECLARE_IMPORT(4, sceSifDma2Init)
-#define I_sceSifInit DECLARE_IMPORT(5, sceSifInit)
-#define I_sceSifSetDChain DECLARE_IMPORT(6, sceSifSetDChain)
-#define I_sceSifSetDma DECLARE_IMPORT(7, sceSifSetDma)
-#define I_sceSifDmaStat DECLARE_IMPORT(8, sceSifDmaStat)
-#define I_sceSifSetOneDma DECLARE_IMPORT(9, sceSifSetOneDma);
-#define I_sceSifSendSync DECLARE_IMPORT(10, sceSifSendSync);
-#define I_sceSifIsSending DECLARE_IMPORT(11, sceSifIsSending);
-#define I_sceSifDma0Transfer DECLARE_IMPORT(12, sceSifDma0Transfer)
-#define I_sceSifDma0Sync DECLARE_IMPORT(13, sceSifDma0Sync)
-#define I_sceSifDma0Sending DECLARE_IMPORT(14, sceSifDma0Sending)
-#define I_sceSifDma1Transfer DECLARE_IMPORT(15, sceSifDma1Transfer)
-#define I_sceSifDma1Sync DECLARE_IMPORT(16, sceSifDma1Sync)
-#define I_sceSifDma1Sending DECLARE_IMPORT(17, sceSifDma1Sending)
-#define I_sceSifDma2Transfer DECLARE_IMPORT(18, sceSifDma2Transfer)
-#define I_sceSifDma2Sync DECLARE_IMPORT(19, sceSifDma2Sync)
-#define I_sceSifDma2Sending DECLARE_IMPORT(20, sceSifDma2Sending)
-#define I_sceSifGetMSFlag DECLARE_IMPORT(21, sceSifGetMSFlag)
-#define I_sceSifSetMSFlag DECLARE_IMPORT(22, sceSifSetMSFlag)
-#define I_sceSifGetSMFlag DECLARE_IMPORT(23, sceSifGetSMFlag)
-#define I_sceSifSetSMFlag DECLARE_IMPORT(24, sceSifSetSMFlag)
-#define I_sceSifGetMainAddr DECLARE_IMPORT(25, sceSifGetMainAddr)
-#define I_sceSifGetSubAddr DECLARE_IMPORT(26, sceSifGetSubAddr)
-#define I_sceSifSetSubAddr DECLARE_IMPORT(27, sceSifSetSubAddr)
-#define I_sceSifIntrMain DECLARE_IMPORT(28, sceSifIntrMain)
-#define I_sceSifCheckInit DECLARE_IMPORT(29, sceSifCheckInit)
-#define I_sceSifSetDmaIntrHandler DECLARE_IMPORT(30, sceSifSetDmaIntrHandler)
-#define I_sceSifResetDmaIntrHandler DECLARE_IMPORT(31, sceSifResetDmaIntrHandler)
-#define I_sceSifSetDmaIntr DECLARE_IMPORT(32, sceSifSetDmaIntr)
-
 #endif /* __SIFMAN_H__ */
