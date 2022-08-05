91c1a6d4335d7609283737958b77ec20b5d101e4
Ziemas <ziemas@ziemas.se>
2022-08-05 10:15:57 +0200

    secrman/sifcmd

--- iop/kernel/include/secrman.h
+++ iop/kernel/include/secrman.h
@@ -30,27 +30,4 @@ void SecrSetMcDevIDHandler(McDevIDHandler_t handler);
 int SecrAuthCard(int port, int slot, int cnum);
 void SecrResetAuthCard(int port, int slot, int cnum);
 
-#define secrman_IMPORTS_start DECLARE_IMPORT_TABLE(secrman, 1, 3)
-#define secrman_IMPORTS_end END_IMPORT_TABLE
-
-#define I_SecrSetMcCommandHandler DECLARE_IMPORT(4, SecrSetMcCommandHandler)
-#define I_SecrSetMcDevIDHandler DECLARE_IMPORT(5, SecrSetMcDevIDHandler)
-#define I_SecrAuthCard DECLARE_IMPORT(6, SecrAuthCard)
-#define I_SecrResetAuthCard DECLARE_IMPORT(7, SecrResetAuthCard)
-
-#define I_SecrCardBootHeader DECLARE_IMPORT(8, SecrCardBootHeader)
-#define I_SecrCardBootBlock DECLARE_IMPORT(9, SecrCardBootBlock)
-#define I_SecrCardBootFile DECLARE_IMPORT(10, SecrCardBootFile)
-#define I_SecrDiskBootHeader DECLARE_IMPORT(11, SecrDiskBootHeader)
-#define I_SecrDiskBootBlock DECLARE_IMPORT(12, SecrDiskBootBlock)
-#define I_SecrDiskBootFile DECLARE_IMPORT(13, SecrDiskBootFile)
-
-/* FOLLOWING EXPORTS ARE ONLY AVAILABLE IN SPECIAL SECRMAN OR FREESECR */
-#define I_SecrDownloadHeader DECLARE_IMPORT(14, SecrDownloadHeader)
-#define I_SecrDownloadBlock DECLARE_IMPORT(15, SecrDownloadBlock)
-#define I_SecrDownloadFile DECLARE_IMPORT(16, SecrDownloadFile)
-#define I_SecrDownloadGetKbit DECLARE_IMPORT(17, SecrDownloadGetKbit)
-#define I_SecrDownloadGetKc DECLARE_IMPORT(18, SecrDownloadGetKc)
-#define I_SecrDownloadGetICVPS2 DECLARE_IMPORT(19, SecrDownloadGetICVPS2)
-
 #endif /* IOP_SECRMAN_H */
--- iop/kernel/include/sifcmd.h
+++ iop/kernel/include/sifcmd.h
@@ -189,34 +189,4 @@ unsigned int isceSifSendCmdIntr(unsigned int, void *, int, void *, void *, int,
 
 #define SifRpcGetOtherData sceSifGetOtherData
 
-#define sifcmd_IMPORTS_start DECLARE_IMPORT_TABLE(sifcmd, 1, 1)
-#define sifcmd_IMPORTS_end END_IMPORT_TABLE
-
-#define I_sceSifInitCmd DECLARE_IMPORT(4, sceSifInitCmd)
-#define I_sceSifExitCmd DECLARE_IMPORT(5, sceSifExitCmd)
-#define I_sceSifGetSreg DECLARE_IMPORT(6, sceSifGetSreg)
-#define I_sceSifSetSreg DECLARE_IMPORT(7, sceSifSetSreg)
-#define I_sceSifSetCmdBuffer DECLARE_IMPORT(8, sceSifSetCmdBuffer)
-#define I_sceSifSetSysCmdBuffer DECLARE_IMPORT(9, sceSifSetSysCmdBuffer)
-#define I_sceSifAddCmdHandler DECLARE_IMPORT(10, sceSifAddCmdHandler)
-#define I_sceSifRemoveCmdHandler DECLARE_IMPORT(11, sceSifRemoveCmdHandler)
-#define I_sceSifSendCmd DECLARE_IMPORT(12, sceSifSendCmd)
-#define I_isceSifSendCmd DECLARE_IMPORT(13, isceSifSendCmd)
-#define I_sceSifInitRpc DECLARE_IMPORT(14, sceSifInitRpc)
-#define I_sceSifBindRpc DECLARE_IMPORT(15, sceSifBindRpc)
-#define I_sceSifCallRpc DECLARE_IMPORT(16, sceSifCallRpc)
-#define I_sceSifRegisterRpc DECLARE_IMPORT(17, sceSifRegisterRpc)
-#define I_sceSifCheckStatRpc DECLARE_IMPORT(18, sceSifCheckStatRpc)
-#define I_sceSifSetRpcQueue DECLARE_IMPORT(19, sceSifSetRpcQueue)
-#define I_sceSifGetNextRequest DECLARE_IMPORT(20, sceSifGetNextRequest)
-#define I_sceSifExecRequest DECLARE_IMPORT(21, sceSifExecRequest)
-#define I_sceSifRpcLoop DECLARE_IMPORT(22, sceSifRpcLoop)
-#define I_sceSifGetOtherData DECLARE_IMPORT(23, sceSifGetOtherData)
-#define I_sceSifRemoveRpc DECLARE_IMPORT(24, sceSifRemoveRpc)
-#define I_sceSifRemoveRpcQueue DECLARE_IMPORT(25, sceSifRemoveRpcQueue)
-#define I_sceSifSetSif1CB DECLARE_IMPORT(26, sceSifSetSif1CB)
-#define I_sceSifClearSif1CB DECLARE_IMPORT(27, sceSifClearSif1CB)
-#define I_sceSifSendCmdIntr DECLARE_IMPORT(28, sceSifSendCmdIntr)
-#define I_isceSifSendCmdIntr DECLARE_IMPORT(29, isceSifSendCmdIntr)
-
 #endif	/* __SIFCMD_H__ */
