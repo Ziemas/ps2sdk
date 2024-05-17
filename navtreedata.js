c67ed4a1419c825a2eb8c6bec502ee3a77b5f084
Ziemas <Ziemas@users.noreply.github.com>
2024-05-17 22:30:47 +0000

    deploy: 5e6e158335cab04262434c41ec26c41ee7fa0093

---
+++ navtreedata.js
@@ -0,0 +1,87 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_2kernel_2_r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_2rpc_2remote_2remote.html", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"atahw_8h.html#a414747b879d890c1df55ccbd4208cf1cad5e246c6f303424f9ba639a2c9717aa4",
+"audsrv__rpc_8h.html",
+"dir_20e91a8bae2e1f802bcb7f8435cbf723.html",
+"dir_ae48e1b29bfa2db4ecbd893188b24955.html",
+"draw_8h.html#a61e54e3ced06afdb40521efcaade8b9b",
+"ee__regs_8h.html#a1153191232b7b6dab6118dc476335521",
+"errno_8h.html#a4a49ca5a72811f9950d2773806eaa641",
+"fsck-ioctl_8h_source.html",
+"graph__vram_8h.html#ae147df9bffc1ec63ac98ba4c9a780847",
+"group__ps2netfs.html#ga26ed3d06ae7b9a0e288270072eb2510f",
+"hwbp_8h.html#aff19de79c165dfde0049a3ddf6ed786e",
+"iop_2kernel_2include_2sifcmd_8h.html#abe81355fa807f8c5646db7a1955f1037",
+"iop__mmio__hwport_8h.html#structssbus2__mmio__hwport__",
+"libcdvd-common_8h.html#aab6ada6614357d110ba1b88d023ebb58",
+"libgs_8h.html#a65e74667e09144c3f97f22932bc5423f",
+"libgs_8h.html#struct_g_s___r___a_l_p_h_a",
+"libmc_8h.html#aad326783042d2ec3f554735b4f23537fa120b04846b0fe0137302d0c341303e2c",
+"loadcore_8h.html#structiop__library__table__t",
+"osd__config_8h.html#a9384226194da7a215d7bcfcf5fe583f9af8faa3fce731bd6ca73bf507274d4192",
+"ps2cam_8h.html#a74a74f7589751469cbb9700d80910f0e",
+"s__cb_8c_source.html",
+"sio_8h.html#ac476bb6dd5d0008911c2c341a16ea398",
+"sssm_8c_source.html",
+"thmsgbx_8h.html#structiop__mbx__t",
+"vm__stav_8c_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/classes_15.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['uart16550_5fregs_5f_0',['uart16550_regs_',['../structuart16550__regs__.html',1,'']]],
+  ['upsample_5ft_1',['upsample_t',['../upsamplers_8h.html#structupsample__t',1,'']]],
+  ['usb_5fmmio_5fhwport_5f_2',['usb_mmio_hwport_',['../iop__mmio__hwport_8h.html#structusb__mmio__hwport__',1,'']]],
+  ['usbconfigdescriptor_3',['UsbConfigDescriptor',['../usbd_8h.html#struct_usb_config_descriptor',1,'']]],
+  ['usbdconfig_4',['UsbdConfig',['../usbdpriv_8h.html#struct_usbd_config',1,'']]],
+  ['usbdevicedescriptor_5',['UsbDeviceDescriptor',['../usbd_8h.html#struct_usb_device_descriptor',1,'']]],
+  ['usbdevicerequest_6',['UsbDeviceRequest',['../usbd_8h.html#struct_usb_device_request',1,'']]],
+  ['usbendpointdescriptor_7',['UsbEndpointDescriptor',['../usbd_8h.html#struct_usb_endpoint_descriptor',1,'']]],
+  ['usbhubdescriptor_8',['UsbHubDescriptor',['../usbd_8h.html#struct_usb_hub_descriptor',1,'']]],
+  ['usbinterfacedescriptor_9',['UsbInterfaceDescriptor',['../usbd_8h.html#struct_usb_interface_descriptor',1,'']]],
+  ['usbmass_5fcmd_10',['usbmass_cmd',['../structusbmass__cmd.html',1,'']]],
+  ['usbmassdeviceinfo_11',['UsbMassDeviceInfo',['../usbhdfsd_8h.html#struct_usb_mass_device_info',1,'']]],
+  ['usbstringdescriptor_12',['UsbStringDescriptor',['../usbd_8h.html#struct_usb_string_descriptor',1,'']]]
+];
